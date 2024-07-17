32804f3dd0ce79badd935e91a61238b45239fe54
Yulchikss <yuliapelia19@gmail.com>
2024-07-17 16:42:58 +0300

    feat: added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/foodies-NextJS",
-    output: "export",  // <=== enables static exports
+  
     reactStrictMode: true,
+
 }
 
 module.exports = nextConfig
