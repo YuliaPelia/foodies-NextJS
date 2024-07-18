b9ecc5dfa4b7fa76e89019ee3de19c5497d96cc1
Yulchikss <yuliapelia19@gmail.com>
2024-07-18 17:21:47 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    // output: "export",
     distDir: 'dist',
-    images: {
-        unoptimized: true
-    }
+    // images: {
+    //     // unoptimized: true
+    // }
 }
 
 module.exports = nextConfig
