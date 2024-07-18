52c3aadfc0e57f0172d9c86b3a450c81caf160ae
Yulchikss <yuliapelia19@gmail.com>
2024-07-18 17:35:45 +0300

    save

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,27 @@
 
 
-export default function MealsPostPage({ params }) {
-    return (
-        <>
-            <h1>Meals Page</h1>
-            <p>{params.mealSlug}</p>
-        </>
-    )
-}
+// export default function MealsPostPage({ params }) {
+//     return (
+//         <>
+//             <h1>Meals Page</h1>
+//             <p>{params.mealSlug}</p>
+//         </>
+//     )
+// }
+
+
+// Return a list of `params` to populate the [slug] dynamic segment
+export async function generateStaticParams() {
+    const posts = await fetch('https://.../posts').then((res) => res.json())
+   
+    return posts.map((post) => ({
+      slug: post.slug,
+    }))
+  }
+   
+  // Multiple versions of this page will be statically generated
+  // using the `params` returned by `generateStaticParams`
+  export default function Page({ params }) {
+    const { slug } = params
+    // ...
+  }
