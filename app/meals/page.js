d96cbd72719818d871457698cab765e91ababdb8
Yulchikss <yuliapelia19@gmail.com>
2024-07-18 17:32:00 +0300

    🥳

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,40 +1,8 @@
-
-import { useRouter } from 'next/navigation';
-
-const MealPage = ({ meal }) => {
-    const router = useRouter();
-
+export default function MealsPage() {
     return (
-        <div>
-            <h1>{meal.name}</h1>
-            <p>{meal.description}</p>
-        </div>
-    );
-};
-
-export async function generateStaticParams() {
-    // Отримайте дані для страв
-    const response = await fetch('https://api.example.com/meals');
-    const meals = await response.json();
-
-    // Поверніть шляхи для кожної страви
-    const paths = meals.map(meal => ({
-        params: { mealSlug: meal.slug },
-    }));
-
-    return paths;
+        <>
+            <h1>Meals Page</h1>
+         
+        </>
+    )
 }
-
-export async function getStaticProps({ params }) {
-    // Отримайте дані для конкретної страви
-    const response = await fetch(`https://api.example.com/meals/${params.mealSlug}`);
-    const meal = await response.json();
-
-    return {
-        props: {
-            meal,
-        },
-    };
-}
-
-export default MealPage;
